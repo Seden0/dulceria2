9b5da7127a1957dbedc2c37f7dc686b1b558f982
carlosvaz777 <carlos.enrique.3em@gmail.com>
2023-07-15 08:59:54 -0600

    Se suben cambios de usuarios

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(require('./routes/index'));
-//app.use(require('./routes/users'));
+app.use(require('./routes/users'));
 //app.use(require('./routes/login'));
 //app.use(require('./routes/signup'));
 //app.use(require('./routes/resetPassw'));
